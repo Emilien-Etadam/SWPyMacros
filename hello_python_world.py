bad863740bd4f377588dd178ef515d0c34ec96cc
émilien <56485277+Emilien-Etadam@users.noreply.github.com>
2023-08-03 15:05:15 +0200

    Update hello_python_world.py
    If Solidworks is not open it open it
    If there is no open part open it create a new part
    Then it write Hello Python

--- hello_python_world.py
+++ hello_python_world.py
@@ -1,22 +1,40 @@
 import sys
 import time
 import asyncio
-
 import pythoncom
-
 import win32com.client
 
-# Solidworks Version (write the year) :
-SWV = 2022
+# Solidworks Version (write the year):
+SWV = 2023
 # API Version
-SWAV = SWV-1992
+SWAV = SWV - 1992
 # Elapsed time
 elapsed = 0
 
 async def main():
-    sw = win32com.client.Dispatch("SldWorks.Application.{}".format(SWAV)) 
-    print(f" Solidworks API Version : {SWAV}","\n",f"Solidworks Version : {SWV}")
+    # Attempt to connect to SolidWorks
+    try:
+        sw = win32com.client.GetActiveObject("SldWorks.Application")
+    except:
+        print("Failed to connect. Launching a new instance.")
+        sw = win32com.client.Dispatch("SldWorks.Application")
+        sw.Visible = True
+
+    if not sw:
+        print("Failed to connect or launch SolidWorks")
+        return
+
+    print(f"Solidworks API Version: {SWAV}", "\n", f"Solidworks Version: {SWV}")
+
     Model = sw.ActiveDoc
+
+    if Model is None:
+        print("No active document found. Creating a new document...")
+        # Create a new part document (you can change the document type)
+        partTemplate = "C:\\ProgramData\\SOLIDWORKS\\SOLIDWORKS " + str(SWV) + "\\templates\\Part.prtdot"
+        Model = sw.NewDocument(partTemplate, 0, 0, 0) # You can adjust the last three parameters as needed
+
+
     ARG_NULL = win32com.client.VARIANT(pythoncom.VT_DISPATCH, None)
     ck = Model.Extension.SelectByID2("Front", "PLANE", 0, 0, 0, False, 0, ARG_NULL, 0)
     Model.SketchManager.InsertSketch(True)
@@ -26,7 +44,7 @@ async def main():
         0, 0, 0, "Hello Python World!", 0, 0, 0, 100, 100
     )
     myFeature = Model.FeatureManager.FeatureExtrusion2(
-        True, 
+        True,
         False,
         False,
         0,
@@ -61,7 +79,7 @@ async def main():
         Model.ViewRotateplusy()
         time.sleep(0.1)
 
-
+# If running as a script, execute the main function
 try:
     if __name__ == "__main__":
         asyncio.run(main())
